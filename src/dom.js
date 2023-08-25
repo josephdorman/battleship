7254e7b0ad0f281896bb235229bf7e2086e24e22
Joseph Dorman <JDorman555@yahoo.com>
2023-08-24 19:24:41 -0500

    func to show ships on grid

--- src/dom.js
+++ src/dom.js
@@ -25,11 +25,26 @@ const dom = (() => {
       for (let h = 0; h < 10; h++) {
         if (board.board[i+1][h+1] != null) {
           const square = document.getElementById(`${i}${h}`);
-          square.textContent = 'Ship Here';
+
+          if (board.board[i+1][h+1] === board.fleet.carrier) {
+            square.textContent = 'Carrier';
+          }
+          else if (board.board[i+1][h+1] === board.fleet.battleship) {
+            square.textContent = 'Battleship';
+          }
+          else if (board.board[i+1][h+1] === board.fleet.cruiser) {
+            square.textContent = 'Cruiser';
+          }
+          else if (board.board[i+1][h+1] === board.fleet.submarine) {
+            square.textContent = 'Submarine';
+          }
+          else {
+            square.textContent = 'Destroyer';
+          }
+          
         }
       }
     }
-
     
   }
 
