4566cef1b8b0ccb0e807f26d2526f69a38ba5328
Joseph Dorman <JDorman555@yahoo.com>
2023-08-29 18:20:53 -0500

    func: to set player name

--- src/handlers.js
+++ src/handlers.js
@@ -1,8 +1,11 @@
 import dom from "./dom";
+import player from "./player";
 
 const handlers = (() => {
   const aiGrid = document.querySelector('.grid-computer')
   const squares = aiGrid.querySelectorAll('.square');
+  const formSubmit = document.getElementById('modal-submit');
+  const name = document.getElementById('name');
 
   function gridClickHandler (id) {
     const coord1 = id.slice(0, 1);
@@ -11,11 +14,19 @@ const handlers = (() => {
     dom.placeShot(coord1, coord2);
   }
 
+  function formClickHandler (value) {
+    player.setName(value);
+  }
+
  squares.forEach(square => {
     square.addEventListener('click', (e) => {
       gridClickHandler(e.target.attributes.aiid.value);
     });
   });
+
+formSubmit.addEventListener('click', () => {
+  formClickHandler(name.value);
+});
 
 })();
 
