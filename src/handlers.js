07481dafa22f55a7148a57b5a594d7e886e08d73
Joseph Dorman <JDorman555@yahoo.com>
2023-09-05 19:25:50 -0500

    change variable

--- src/handlers.js
+++ src/handlers.js
@@ -17,7 +17,7 @@ const handlers = (() => {
   function gridClickHandler (id) {
     const coord1 = id.slice(0, 1);
     const coord2 = id.slice(1, 2);
-    dom.aiBoard.receiveAttack(coord1, coord2);
+    ai.board.receiveAttack(coord1, coord2);
     dom.placeShot(coord1, coord2);
   }
 
