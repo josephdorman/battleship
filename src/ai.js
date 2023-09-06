1c395f030c9f7c60d633eb95efe46013dd48b079
Joseph Dorman <JDorman555@yahoo.com>
2023-09-05 19:51:15 -0500

    generate possible shots for ai

--- src/ai.js
+++ src/ai.js
@@ -8,6 +8,27 @@ const ai = (() => {
   board.placeShip('submarine', 'hor', 3, 3);
   board.placeShip('destroyer', 'hor', 2, 3);
 
+  const possibleShots = []; // '01', '02', '03', '04', '05', '06', '07', '08', '09'
+  const currentShots = [];
+
+  function generatePossibleShots () {
+    for (let i = 0; i < 10; i++) {
+      possibleShots.push(`0${i}`);
+    }
+
+    for (let i = 10; i < 99; i++) {
+      possibleShots.push(i);
+    }
+
+    console.log(possibleShots);
+  }
+
+  function generateShot () {
+
+  }
+
+  generatePossibleShots();
+
   return {board};
 
 })();
